f812a0ea64f6f9bc40cc7f8e090fbf70f0d0ca8c
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-12 09:51:12 +0100

    UD5_1_if

---
+++ src/mayorAPP.java
@@ -0,0 +1,18 @@
+
+public class mayorAPP {
+
+	public static void main(String[] args) {
+		
+		int varA=21, varB=20;
+		
+		if(varA > varB) {
+			System.out.println("A es mayor que B");
+		}
+		else if(varA < varB) {
+			System.out.println("A es menor que B");
+		}
+		else {
+			System.out.println("A es igual que B");
+		}
+	}
+}
